877fd72fcacc47eb1254458f447a7a48b0c210f0
cadonj12 <cadonjohnson21@gmail.com>
2024-04-30 16:16:05 -0700

    question 3

--- Advanced List Methods and Identity Operators.py
+++ Advanced List Methods and Identity Operators.py
@@ -5,3 +5,19 @@ attended = ["Charlie", "Eve", "Alice", "Frank"]
 
 good_students = [student for student in submitted if student in attended]
 print(good_students)
+
+
+#Task 2
+
+submitted.sort()
+attended.sort()
+
+are_identical = submitted == attended
+print(are_identical)
+
+
+#Task 3
+
+attended.remove("Eve")
+attended.remove("Frank")
+print(attended)
