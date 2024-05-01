f99bac105b644b5dd67307813bb0371e74e9f5f1
cadonj12 <cadonjohnson21@gmail.com>
2024-04-30 19:18:13 -0700

    question 4

---
+++ Deep Dive into Python Lists.py
@@ -0,0 +1,20 @@
+students = ["John", "Doe", "Jane", "Smith"]
+grades = [85, 90, 78, 88]
+activities = ["Football", "Music", "Art", "Dance"]
+
+
+#Task 1
+
+for i in range(len(students)):
+    if grades[i] < 80:
+        print(students[i], grades[i], activities[i])
+
+
+#Task 2
+
+students_approved = ["John", "Doe", "Smith"]
+
+
+#Task 3
+
+print(students_approved)
